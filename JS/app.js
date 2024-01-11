48c42d7cf8b79495f65781a6aafa280510897def
AaronDevX <154556556+AaronDevX@users.noreply.github.com>
2024-01-11 18:57:35 -0500

    Implement form validation with corresponding alert display

--- JS/app.js
+++ JS/app.js
@@ -33,6 +33,35 @@ function registerData(e){
     console.log(petObject)
 }
 
+class UI{
+    showAlert(type, message){
+        const divParent = document.querySelector(".container");
+
+        const divAlert = document.createElement("DIV");
+        const pAlert = document.createElement("P");
+        pAlert.classList.add(type, "alert");
+        pAlert.textContent = message;
+
+        divAlert.appendChild(pAlert);
+        divParent.insertBefore(divAlert, document.querySelector("#content"));
+
+        setTimeout(() => {
+            divAlert.remove();
+        }, 3000);
+    }
+}
+
+const uInterface = new UI;
+
 function sendForm(e){
     e.preventDefault();
+
+    const {pet, owner, phone, date, time, symptoms} = petObject;
+
+    if(pet=="" || owner=="" || phone=="" || date=="" || time=="" || symptoms==""){
+        uInterface.showAlert("error-alert", "All fields are required");
+        return;
+    }
+
+    uInterface.showAlert("success-alert", "Patient added");
 }
